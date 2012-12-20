a9b75f533f426067b66ff726adf8ca285d46ef26
Eyal Lotem <eyal.lotem@gmail.com>
2012-12-20 14:39:22 +0200

    Remove empty tests

--- Setup.hs
+++ Setup.hs
@@ -1,7 +1,2 @@
-import Control.Monad (void)
 import Distribution.Simple
-import System.Cmd (system)
-
-main = defaultMainWithHooks $ simpleUserHooks { runTests = runElfTests }
-
-runElfTests a b pd lb = void $ system "runhaskell -i./src ./tests/Test.hs"
+main = defaultMain
